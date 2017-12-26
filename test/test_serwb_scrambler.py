cb69afd3f81ec4c44ef4e565e1c7acca18052754
Florent Kermarrec <florent@enjoy-digital.fr>
2017-12-26 07:28:57 +0100

    test/test_serwb_scrambler: fix & cleanup

--- test/test_serwb_scrambler.py
+++ test/test_serwb_scrambler.py
@@ -4,22 +4,23 @@ from litex.gen import *
 
 from liteiclink.serwb import scrambler
 
-from litex.soc.interconnect.wishbone import SRAM
-from litex.soc.interconnect.stream import Converter
-
 
 class DUT(Module):
     def __init__(self):
-        self.submodules.scrambler = scrambler.Scrambler()
+        self.submodules.scrambler = scrambler.Scrambler(sync_interval=16)
         self.submodules.descrambler = scrambler.Descrambler()
-        self.comb += self.scrambler.source.connect(self.descrambler.sink)
+        self.comb += [
+            self.scrambler.source.connect(self.descrambler.sink),
+            self.descrambler.source.ready.eq(1)
+        ]
 
 
 def main_generator(dut):
     i = 0
     errors = 0
     last_data = -1
-    while i != 2048:
+    while i != 256:
+        print(i)
         # stim
         if (yield dut.scrambler.sink.ready):
             i += 1
