63ecdf0f925919f6bbdbf1d48947d6a9125bc5da
Florent Kermarrec <florent@enjoy-digital.fr>
2018-10-31 08:19:13 +0100

    examples/transceiver: add pcie_cpri example

---
+++ examples/transceiver/pcie_cpri.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+import sys
+
+from migen import *
+from migen.genlib.io import CRG
+
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+
+from liteiclink.transceiver.gtp_7series import GTPQuadPLL, GTP
+
+
+_io = [
+    ("clk100", 0, Pins("R4"), IOStandard("LVCMOS25")),
+    ("rst_n", Pins("AA1"), IOStandard("LVCMOS25")),
+
+    ("user_led", 0, Pins("AB1"), IOStandard("LVCMOS25")),
+    ("user_led", 1, Pins("AB8"), IOStandard("LVCMOS25")),
+
+    ("user_btn", 0, Pins("AA1"), IOStandard("LVCMOS25")),
+    ("user_btn", 1, Pins("AB6"), IOStandard("LVCMOS25")),
+
+    ("serial", 0,
+        Subsignal("tx", Pins("Y6")),
+        Subsignal("rx", Pins("AA6")),
+        IOStandard("LVCMOS25")
+    ),
+
+    ("sfp_tx_disable_n", 0, Pins("AA20"), IOStandard("LVCMOS25")),
+    ("sfp_tx", 0,
+        Subsignal("p", Pins("D5")),
+        Subsignal("n", Pins("C5")),
+    ),
+    ("sfp_rx", 0,
+        Subsignal("p", Pins("D11")),
+        Subsignal("n", Pins("C11")),
+    ),
+
+    ("sfp_tx_disable_n", 1, Pins("V17"), IOStandard("LVCMOS25")),
+    ("sfp_tx", 1,
+        Subsignal("p", Pins("D7")),
+        Subsignal("n", Pins("C7")),
+    ),
+    ("sfp_rx", 1,
+        Subsignal("p", Pins("D9")),
+        Subsignal("n", Pins("C9")),
+    ),
+]
+
+
+class Platform(XilinxPlatform):
+    def __init__(self):
+        XilinxPlatform.__init__(self, "xc7a50t-fgg484-2", _io, toolchain="vivado")
+
+
+class GTPTestSoC(SoCCore):
+    def __init__(self, platform, medium="sfp0"):
+        sys_clk_freq = int(100e6)
+        SoCCore.__init__(self, platform, sys_clk_freq, cpu_type=None)
+        clk100 = platform.request("clk100")
+        rst = ~platform.request("user_btn", 0)
+        self.submodules.crg = CRG(clk100, rst)
+
+        # refclk
+        refclk150 = Signal()
+        refclk150_bufg = Signal()
+        pll_fb = Signal()
+        self.specials += [
+            Instance("PLLE2_BASE",
+                p_STARTUP_WAIT="FALSE", #o_LOCKED=,
+
+                # VCO @ 1.2GHz
+                p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=10.0,
+                p_CLKFBOUT_MULT=12, p_DIVCLK_DIVIDE=1,
+                i_CLKIN1=ClockSignal(), i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb,
+
+                # 150MHz
+                p_CLKOUT0_DIVIDE=8, p_CLKOUT0_PHASE=0.0, o_CLKOUT0=refclk150
+            ),
+            Instance("BUFG", i_I=refclk150, o_O=refclk150_bufg)
+        ]
+        platform.add_platform_command("set_property SEVERITY {{Warning}} [get_drc_checks REQP-49]")
+
+        # pll
+        qpll = GTPQuadPLL(refclk150_bufg, 150e6, 3.0e9)
+        print(qpll)
+        self.submodules += qpll
+
+        # gtp
+        if medium == "sfp0":
+            self.comb += platform.request("sfp_tx_disable_n", 0).eq(1)
+            tx_pads = platform.request("sfp_tx", 0)
+            rx_pads = platform.request("sfp_rx", 0)
+        elif medium == "sfp1":
+            self.comb += platform.request("sfp_tx_disable_n", 1).eq(1)
+            tx_pads = platform.request("sfp_tx", 1)
+            rx_pads = platform.request("sfp_rx", 1)
+        else:
+            raise ValueError
+        gtp = GTP(qpll, tx_pads, rx_pads, sys_clk_freq,
+            clock_aligner=True, internal_loopback=False)
+        self.submodules += gtp
+
+        # led blink
+        counter = Signal(32)
+        self.sync.tx += counter.eq(counter + 1)
+
+        self.comb += [
+            gtp.encoder.k[0].eq(1),
+            gtp.encoder.d[0].eq((5 << 5) | 28),
+            gtp.encoder.k[1].eq(0)
+        ]
+
+        self.crg.cd_sys.clk.attr.add("keep")
+        gtp.cd_tx.clk.attr.add("keep")
+        gtp.cd_rx.clk.attr.add("keep")
+        platform.add_period_constraint(self.crg.cd_sys.clk, 10)
+        platform.add_period_constraint(gtp.cd_tx.clk, 1e9/gtp.tx_clk_freq)
+        platform.add_period_constraint(gtp.cd_rx.clk, 1e9/gtp.tx_clk_freq)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            gtp.cd_tx.clk)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            gtp.cd_rx.clk)
+
+
+        tx_counter_led = Signal()
+        tx_counter = Signal(32)
+        self.sync.tx += tx_counter.eq(tx_counter + 1)
+        self.comb += tx_counter_led.eq(tx_counter[26])
+
+        rx_counter_led = Signal()
+        rx_counter = Signal(32)
+        self.sync.rx += rx_counter.eq(rx_counter + 1)
+        self.comb += rx_counter_led.eq(rx_counter[26])
+
+        self.comb += platform.request("user_led", 0).eq(tx_counter_led)
+        self.comb += platform.request("user_led", 1).eq(rx_counter_led)
+
+
+def main():
+    if "load" in sys.argv[1:]:
+        from litex.build.xilinx import VivadoProgrammer
+        prog = VivadoProgrammer()
+        prog.load_bitstream("build/gateware/pcie_cpri.bit")
+    else:
+        platform = Platform()
+        soc = GTPTestSoC(platform)
+        builder = Builder(soc, output_dir="build", compile_gateware=True)
+        vns = builder.build(build_name="pcie_cpri")
+
+
+if __name__ == "__main__":
+    main()
