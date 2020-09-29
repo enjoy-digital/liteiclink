63987bf4e2d680518eb2e15aafe46163bc7f7b92
Florent Kermarrec <florent@enjoy-digital.fr>
2020-09-29 12:29:25 +0200

    examples/serwb/nexys: update.

--- examples/serwb/nexys.py
+++ examples/serwb/nexys.py
@@ -7,6 +7,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 import sys
+import argparse
 
 from migen import *
 from migen.genlib.misc import WaitTimer
@@ -28,42 +29,44 @@ from liteiclink.serwb.core import SERWBCore
 
 from litescope import LiteScopeAnalyzer
 
+# IOs ----------------------------------------------------------------------------------------------
 
 serwb_io = [
     # hdmi loopback
     ("serwb_master", 0,
         Subsignal("clk_p", Pins("T1"), IOStandard("TMDS_33")), # hdmi_out clk
         Subsignal("clk_n", Pins("U1"), IOStandard("TMDS_33")), # hdmi_out clk
-        Subsignal("tx_p", Pins("W1"), IOStandard("TMDS_33")),  # hdmi_out data0
-        Subsignal("tx_n", Pins("Y1"), IOStandard("TMDS_33")),  # hdmi_out data0
-        Subsignal("rx_p", Pins("W2"), IOStandard("TMDS_33")),  # hdmi_in data1
-        Subsignal("rx_n", Pins("Y2"), IOStandard("TMDS_33")),  # hdmi_in data1
+        Subsignal("tx_p",  Pins("W1"), IOStandard("TMDS_33")), # hdmi_out data0
+        Subsignal("tx_n",  Pins("Y1"), IOStandard("TMDS_33")), # hdmi_out data0
+        Subsignal("rx_p",  Pins("W2"), IOStandard("TMDS_33")), # hdmi_in data1
+        Subsignal("rx_n",  Pins("Y2"), IOStandard("TMDS_33")), # hdmi_in data1
     ),
 
     ("serwb_slave", 0,
-        Subsignal("clk_p", Pins("V4"), IOStandard("TMDS_33")), # hdmi_in clk
-        Subsignal("clk_n", Pins("W4"), IOStandard("TMDS_33")), # hdmi_in clk
-        Subsignal("tx_p", Pins("AA1"), IOStandard("TMDS_33")), # hdmi_out data1
-        Subsignal("tx_n", Pins("AB1"), IOStandard("TMDS_33")), # hdmi_out data1
-        Subsignal("rx_p", Pins("Y3"), IOStandard("TMDS_33")),  # hdmi_in data0s
-        Subsignal("rx_n", Pins("AA3"), IOStandard("TMDS_33")), # hdmi_in data0
+        Subsignal("clk_p", Pins("V4"),  IOStandard("TMDS_33")), # hdmi_in clk
+        Subsignal("clk_n", Pins("W4"),  IOStandard("TMDS_33")), # hdmi_in clk
+        Subsignal("tx_p",  Pins("AA1"), IOStandard("TMDS_33")), # hdmi_out data1
+        Subsignal("tx_n",  Pins("AB1"), IOStandard("TMDS_33")), # hdmi_out data1
+        Subsignal("rx_p",  Pins("Y3"),  IOStandard("TMDS_33")), # hdmi_in data0
+        Subsignal("rx_n",  Pins("AA3"), IOStandard("TMDS_33")), # hdmi_in data0
     ),
     ("serwb_enable", 0, Pins("R3"), IOStandard("LVCMOS33")), # hdmi_txen
 ]
 
+# CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(Module):
     def __init__(self, platform):
-        self.clock_domains.cd_sys = ClockDomain()
-        self.clock_domains.cd_sys4x = ClockDomain()
+        self.clock_domains.cd_sys    = ClockDomain()
+        self.clock_domains.cd_sys4x  = ClockDomain()
         self.clock_domains.cd_clk200 = ClockDomain()
 
         clk100 = platform.request("clk100")
-        reset = ~platform.request("cpu_reset")
+        reset  = ~platform.request("cpu_reset")
 
         pll_locked = Signal()
-        pll_fb = Signal()
-        pll_sys4x = Signal()
+        pll_fb     = Signal()
+        pll_sys4x  = Signal()
         pll_clk200 = Signal()
         self.specials += [
             Instance("MMCME2_BASE",
@@ -97,22 +100,24 @@ class _CRG(Module):
             )
         self.specials += Instance("IDELAYCTRL", i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset)
 
+# BaseSoC ------------------------------------------------------------------------------------------
 
-class BaseSoC(SoCCore):
+class BaseSoC(SoCMini):
     def __init__(self, platform):
-        clk_freq = int(125e6)
-        SoCCore.__init__(self, platform, clk_freq,
-            cpu_type=None,
-            csr_data_width=32,
-            with_uart=False,
-            ident="Nexys Video - LiteICLink Test Design -", ident_version=True,
-            with_timer=False
-        )
-        self.submodules.crg = _CRG(platform)
-        self.add_cpu_or_bridge(UARTWishboneBridge(platform.request("serial"),
-                                                  clk_freq, baudrate=3e6))
-        self.add_wb_master(self.cpu_or_bridge.wishbone)
+        sys_clk_freq = int(125e6)
 
+        # SoCMini ----------------------------------------------------------------------------------
+        SoCMini.__init__(self, platform, sys_clk_freq,
+            csr_data_width = 32,
+            ident          = "LiteICLink example design on Nexys Video",
+            ident_version  = True,
+            with_uart      = True,
+            uart_name      = "bridge")
+
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = _CRG(platform)
+
+# SERDESTestSoC ------------------------------------------------------------------------------------
 
 class SERDESTestSoC(BaseSoC):
     csr_map = {
@@ -128,23 +133,22 @@ class SERDESTestSoC(BaseSoC):
     }
     mem_map.update(BaseSoC.mem_map)
 
-    def __init__(self, platform, low_speed=False, with_analyzer=True):
+    def __init__(self, platform, low_speed=True, with_analyzer=True):
         BaseSoC.__init__(self, platform)
 
-        # serwb enable
+        # SerWB ------------------------------------------------------------------------------------
+        phy_cls = SERWBLowSpeedPHY if low_speed else SERWBPHY
+
+        # Enable
         self.comb += platform.request("serwb_enable").eq(1)
 
-        if low_speed:
-            phy_cls = SERWBLowSpeedPHY
-        else:
-            phy_cls = SERWBPHY
-
-        # serwb master
+        # Master
         self.submodules.serwb_master_phy = phy_cls(platform.device, platform.request("serwb_master"), mode="master")
-        # serwb slave
+
+        # Slave
         self.submodules.serwb_slave_phy = phy_cls(platform.device, platform.request("serwb_slave"), mode="slave")
 
-        # leds
+        # Status leds
         self.comb += [
             platform.request("user_led", 4).eq(self.serwb_master_phy.init.ready),
             platform.request("user_led", 5).eq(self.serwb_master_phy.init.error),
@@ -152,20 +156,20 @@ class SERDESTestSoC(BaseSoC):
             platform.request("user_led", 7).eq(self.serwb_slave_phy.init.error),
         ]
 
-        # wishbone slave
+        # Wishbone Slave
         serwb_master_core = SERWBCore(self.serwb_master_phy, self.clk_freq, mode="slave")
         self.submodules += serwb_master_core
 
-        # wishbone master
+        # Wishbone Master
         serwb_slave_core = SERWBCore(self.serwb_slave_phy, self.clk_freq, mode="master")
         self.submodules += serwb_slave_core
 
-        # wishbone test memory
+        # Wishbone Test Memory
         self.register_mem("serwb", self.mem_map["serwb"], serwb_master_core.etherbone.wishbone.bus, 8192)
         self.submodules.serwb_sram = wishbone.SRAM(8192, init=[i for i in range(8192//4)])
         self.comb += serwb_slave_core.etherbone.wishbone.bus.connect(self.serwb_sram.bus)
 
-        # analyzer
+        # Analyzer
         if with_analyzer:
             converter_group = [
                 self.serwb_master_phy.serdes.tx.datapath.converter.sink,
@@ -208,28 +212,25 @@ class SERDESTestSoC(BaseSoC):
                 1 : encoder_group,
                 2 : control_group
             }
-            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 256)
+            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 256, csr_csv="test/analyzer.csv")
 
-    def do_exit(self, vns):
-        if hasattr(self, "analyzer"):
-            self.analyzer.export_csv(vns, "test/analyzer.csv")
+# Build --------------------------------------------------------------------------------------------
 
 def main():
+    parser = argparse.ArgumentParser(description="LiteICLink example design on Nexys Video")
+    parser.add_argument("--build",  action="store_true", help="Build bitstream")
+    parser.add_argument("--load",   action="store_true", help="Load bitstream (to SRAM)")
+    args = parser.parse_args()
+
     platform = nexys.Platform()
     platform.add_extension(serwb_io)
-    if len(sys.argv) < 2:
-        print("missing target (base or serwb)")
-        exit()
-    if sys.argv[1] == "base":
-        soc = BaseSoC(platform)
-    elif sys.argv[1] == "serwb":
-        soc = SERDESTestSoC(platform)
-    else:
-        raise KeyError
-    builder = Builder(soc, output_dir="build", csr_csv="test/csr.csv")
-    vns = builder.build()
-    soc.do_exit(vns)
+    soc      = SERDESTestSoC(platform)
+    builder  = Builder(soc, csr_csv="test/csr.csv")
+    builder.build(run=args.build)
 
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
 
 if __name__ == "__main__":
     main()
