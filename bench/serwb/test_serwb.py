ac54ab9fdf41452a2030ee0f6087b05431049261
Florent Kermarrec <florent@enjoy-digital.fr>
2020-10-16 15:47:22 +0200

    bench/serwb/test_serwb: simplify/rewrite, only keep init and sram tests.

--- bench/serwb/test_serwb.py
+++ bench/serwb/test_serwb.py
@@ -1,60 +1,52 @@
 #!/usr/bin/env python3
+
+#
+# This file is part of LiteICLink.
+#
+# Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+# LiteICLink SerWB test utility.
+
 import sys
 import time
+import argparse
 
 from litex import RemoteClient
 
 from litescope import LiteScopeAnalyzerDriver
 
-wb = RemoteClient()
-wb.open()
 
-# # #
+# Init Test ----------------------------------------------------------------------------------------
 
-def seed_to_data(seed, random=True):
-    if random:
-        return (1664525*seed + 1013904223) & 0xffffffff
-    else:
-        return seed
+def init_test(port):
+    wb = RemoteClient(port=port)
+    wb.open()
 
-def write_pattern(length):
-    for i in range(length):
-        wb.write(wb.mems.serwb.base + 4*i, seed_to_data(i))
-
-def check_pattern(length, debug=False):
-    errors = 0
-    for i in range(length):
-        error = 0
-        read_data = wb.read(wb.mems.serwb.base + 4*i)
-        if read_data != seed_to_data(i):
-            error = 1
-            if debug:
-                print("{}: 0x{:08x}, 0x{:08x}   KO".format(i, read_data, seed_to_data(i)))
-        else:
-            if debug:
-                print("{}: 0x{:08x}, 0x{:08x} OK".format(i, read_data, seed_to_data(i)))
-        errors += error
-    return errors
-
-if len(sys.argv) < 2:
-    print("missing test (init, prbs, scrambling, wishbone, analyzer)")
-    wb.close()
-    exit()
-
-if sys.argv[1] == "init":
+    # Reset SerWB Master
+    print("Reseting SerWB Master...")
     wb.regs.serwb_master_phy_control_reset.write(1)
-    timeout = 40
-    print("SERWB init", end="")
+
+    # Initialize SerWB Master
+    timeout = 4
+    print(f"Initializing SerWB Master...")
     while (wb.regs.serwb_master_phy_control_ready.read() == 0 and
            wb.regs.serwb_master_phy_control_error.read() == 0 and
            timeout > 0):
         time.sleep(0.1)
-        print(".", end="")
+        print(f"{timeout:2.2f}s", end="\r")
         sys.stdout.flush()
-        timeout -= 1
+        timeout -= 0.1
     print("")
-    print("Master configuration")
-    print("-----------------")
+    if (timeout <= 0):
+        print("Failed.")
+        return
+    else:
+        print("Success.")
+
+    # Show Master Config
+    print("Master config")
+    print("-------------")
     if hasattr(wb.regs, "serwb_master_phy_control_delay"):
         print("delay_min_found: {:d}".format(wb.regs.serwb_master_phy_control_delay_min_found.read()))
         print("delay_min: {:d}".format(wb.regs.serwb_master_phy_control_delay_min.read()))
@@ -65,8 +57,10 @@ if sys.argv[1] == "init":
     print("ready: {:d}".format(wb.regs.serwb_master_phy_control_ready.read()))
     print("error: {:d}".format(wb.regs.serwb_master_phy_control_error.read()))
     print("")
-    print("Slave configuration")
-    print("-----------------")
+
+    # Show Slave Config
+    print("Slave config")
+    print("------------")
     if hasattr(wb.regs, "serwb_slave_phy_control_delay"):
         print("delay_min_found: {:d}".format(wb.regs.serwb_slave_phy_control_delay_min_found.read()))
         print("delay_min: {:d}".format(wb.regs.serwb_slave_phy_control_delay_min.read()))
@@ -77,48 +71,72 @@ if sys.argv[1] == "init":
     print("ready: {:d}".format(wb.regs.serwb_slave_phy_control_ready.read()))
     print("error: {:d}".format(wb.regs.serwb_slave_phy_control_error.read()))
 
-elif sys.argv[1] == "prbs":
-    print("PRBS Slave to Master test:")
-    wb.regs.serwb_master_phy_control_prbs_cycles.write(int(1e6))
-    wb.regs.serwb_master_phy_control_prbs_start.write(1)
-    #check_pattern(1, debug=False) # error injecton
-    time.sleep(1)
-    print("errors : %d" %wb.regs.serwb_master_phy_control_prbs_errors.read())
+    wb.close()
 
-    print("PRBS Master to Slave test:")
-    wb.regs.serwb_slave_phy_control_prbs_cycles.write(int(1e6))
-    wb.regs.serwb_slave_phy_control_prbs_start.write(1)
-    #check_pattern(1, debug=False) # error injecton
-    time.sleep(1)
-    print("errors : %d" %wb.regs.serwb_slave_phy_control_prbs_errors.read())
+# SRAM Test ----------------------------------------------------------------------------------------
 
-elif sys.argv[1] == "scrambling":
-    if wb.regs.serwb_master_phy_control_scrambling_enable.read():
-        print("Disabling scrambling")
-        wb.regs.serwb_master_phy_control_scrambling_enable.write(0)
-        wb.regs.serwb_slave_phy_control_scrambling_enable.write(0)
-    else:
-        print("Enabling scrambling")
-        wb.regs.serwb_master_phy_control_scrambling_enable.write(1)
-        wb.regs.serwb_slave_phy_control_scrambling_enable.write(1)
+def sram_test(port):
+    wb = RemoteClient(port=port)
+    wb.open()
 
-elif sys.argv[1] == "wishbone":
-    write_pattern(128)
-    errors = check_pattern(128, debug=False)
-    print("errors: {:d}".format(errors))
+    def mem_dump(base, length):
+        for addr in range(base, base + length, 4):
+            if (addr%16 == 0):
+                if addr != base:
+                    print("")
+                print("0x{:08x}".format(addr), end="  ")
+            data = wb.read(addr)
+            for i in reversed(range(4)):
+                print("{:02x}".format((data >> (8*i)) & 0xff), end=" ")
+        print("")
 
-elif sys.argv[1] == "analyzer":
-    analyzer = LiteScopeAnalyzerDriver(wb.regs, "analyzer", debug=True)
-    analyzer.configure_trigger(cond={})
-    analyzer.run(offset=32, length=256)
+    def mem_write(base, datas):
+        for n, addr in enumerate(range(base, base + 4*len(datas), 4)):
+            if (addr%16 == 0):
+                if addr != base:
+                    print("")
+                print("0x{:08x}".format(addr), end="  ")
+            data = datas[n]
+            for i in reversed(range(4)):
+                print("{:02x}".format((data >> (8*i)) & 0xff), end=" ")
+            wb.write(addr, data)
+        print("")
 
-    analyzer.wait_done()
-    analyzer.upload()
-    analyzer.save("dump.vcd")
 
-else:
-    raise ValueError
+    print("Fill SRAM with counter:")
+    mem_write(wb.mems.serwb.base, [i for i in range(128)])
+    print("")
 
-# # #
+    print("Dump SRAM:")
+    mem_dump(wb.mems.serwb.base, 128)
+    print("")
 
-wb.close()
+    print("Fill SRAM with 4 32-bit words:")
+    mem_write(wb.mems.serwb.base, [0x01234567, 0x89abcdef, 0x5aa55aa5, 0xa55aa55a])
+    print("")
+
+    print("Dump SRAM:")
+    mem_dump(wb.mems.serwb.base, 128)
+    print("")
+
+    wb.close()
+
+# Run ----------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteICLink SerWB test utility")
+    parser.add_argument("--port",  default="1234",      help="Host bind port")
+    parser.add_argument("--init",  action="store_true", help="Initialize SerWB Link")
+    parser.add_argument("--sram",  action="store_true", help="Test SRAM access over SerWB")
+    args = parser.parse_args()
+
+    port = int(args.port, 0)
+
+    if args.init:
+        init_test(port=port)
+
+    if args.sram:
+        sram_test(port=port)
+
+if __name__ == "__main__":
+    main()
