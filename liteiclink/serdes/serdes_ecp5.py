1ef0fff6f36e60a76fd91fcd1bb5644c41058e7b
Sylvain Munaut <tnt@246tNt.com>
2021-12-13 19:55:50 +0100

    test_prbs: Handle PRBS error counter wrap around
    Previously if the PRBS counter reached its max value of 0xffffffff,
    the reported number of errors would drop to 0. And this happens
    pretty quickly given the fast data rates.
    
    Now we enable the new 'wrap' option of PRBSRX core so that the counter
    wraps to 0 on overflows and we detect and handle that condition in
    test_prbs.py
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- liteiclink/serdes/serdes_ecp5.py
+++ liteiclink/serdes/serdes_ecp5.py
@@ -696,7 +696,7 @@ class SerDesECP5(Module, AutoCSR):
         ]
 
         # RX Datapath and PRBS ---------------------------------------------------------------------
-        self.submodules.rx_prbs = ClockDomainsRenamer("rx")(PRBSRX(data_width, True))
+        self.submodules.rx_prbs = ClockDomainsRenamer("rx")(PRBSRX(data_width, True, wrap=True))
         self.comb += [
             self.rx_prbs.config.eq(rx_prbs_config),
             self.rx_prbs.pause.eq(rx_prbs_pause),
