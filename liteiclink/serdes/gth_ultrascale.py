0e06d8aece8228e26b1d9f0e081b021cae49685e
Florent Kermarrec <florent@enjoy-digital.fr>
2023-06-21 10:02:45 +0200

    serdes/gth: Update copyrithts.

--- liteiclink/serdes/gth_ultrascale.py
+++ liteiclink/serdes/gth_ultrascale.py
@@ -1,7 +1,8 @@
 #
 # This file is part of LiteICLink.
 #
-# Copyright (c) 2017-2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2017-2023 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2021 Sylvain Munaut <tnt@246tNt.com>
 # Copyright (c) 2017 Sebastien Bourdeauducq <sb@m-labs.hk>
 # SPDX-License-Identifier: BSD-2-Clause
 
