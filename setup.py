9cf66edd028dc238cc825766fc9c4bbdc084c769
Florent Kermarrec <florent@enjoy-digital.fr>
2023-09-18 08:42:48 +0200

    setup.py: Update to 2023.08.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="liteiclink",
-    version="2023.04",
+    version="2023.08",
     description="Small footprint and configurable Inter-Chip communication cores",
     long_description=long_description,
     long_description_content_type="text/markdown",
