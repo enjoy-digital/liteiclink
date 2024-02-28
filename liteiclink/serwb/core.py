d1d5c642eea6aaa61108f363930314dbea979fc7
Florent Kermarrec <florent@enjoy-digital.fr>
2024-02-28 13:06:13 +0100

    serwb/core: Add multiple downstream/upstream endpoints support and abritration/dispatch to prepare for IOs support.

--- liteiclink/serwb/core.py
+++ liteiclink/serwb/core.py
@@ -1,7 +1,7 @@
 #
 # This file is part of LiteICLink.
 #
-# Copyright (c) 2017-2023 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2017-2024 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
 from migen import *
@@ -9,7 +9,9 @@ from migen import *
 from litex.gen import *
 
 from litex.soc.interconnect import stream
+from litex.soc.interconnect.packet import Arbiter, Dispatcher
 
+from liteiclink.serwb.packet import packet_description
 from liteiclink.serwb.packet import Packetizer, Depacketizer
 from liteiclink.serwb.etherbone import Etherbone
 
@@ -19,7 +21,8 @@ class SERWBCore(LiteXModule):
     def __init__(self, phy, clk_freq, mode,
         etherbone_buffer_depth = 4,
         tx_buffer_depth        = 8,
-        rx_buffer_depth        = 8):
+        rx_buffer_depth        = 8,
+    ):
 
         # Etherbone.
         # ----------
@@ -31,8 +34,8 @@ class SERWBCore(LiteXModule):
 
         # Packetizer / Depacketizer.
         # --------------------------
-        self.depacketizer = depacketizer = Depacketizer(clk_freq)
         self.packetizer   = packetizer   = Packetizer()
+        self.depacketizer = depacketizer = Depacketizer(clk_freq)
 
         # Buffering.
         # ----------
@@ -49,8 +52,35 @@ class SERWBCore(LiteXModule):
 
             phy.source.connect(rx_fifo.sink),
             rx_fifo.source.connect(depacketizer.sink),
-
-            # Etherbone <--> Core
-            depacketizer.source.connect(etherbone.sink, keep={"valid", "ready", "last", "data", "length"}),
-            etherbone.source.connect(packetizer.sink,   keep={"valid", "ready", "last", "data", "length"}),
         ]
+
+        # Downstream/Upstream Endpoints.
+        # ------------------------------
+        self.downstream_endpoints = {0: etherbone.source}
+        self.upstream_endpoints   = {0: etherbone.sink  }
+
+    def do_finalize(self):
+        # Downstream Arbitration.
+        # -----------------------
+        downstream_endpoints = [stream.Endpoint(packet_description(32))] * len(self.downstream_endpoints)
+        for i, (k, v) in enumerate(self.downstream_endpoints.items()):
+            self.comb += [
+                v.connect(downstream_endpoints[i], keep={"valid", "ready", "last", "data", "length"}),
+                downstream_endpoints[i].port.eq(k),
+            ]
+        self.arbiter = Arbiter(
+            masters = downstream_endpoints,
+            slave   = self.packetizer.sink,
+            keep    = {"valid", "ready", "last", "data", "length"},
+        )
+
+        # Upstream Dispatching.
+        # ---------------------
+        self.dispatcher = Dispatcher(
+            master  = self.depacketizer.source,
+            slaves  = [ep for _, ep in self.upstream_endpoints.items()],
+            one_hot = False,
+            keep    = {"valid", "ready", "last", "data", "length"},
+        )
+        for i, (k, v) in enumerate(self.upstream_endpoints.items()):
+            self.comb += If(self.depacketizer.source.port == k, self.dispatcher.sel.eq(i))
--- liteiclink/serwb/packet.py
+++ liteiclink/serwb/packet.py
@@ -1,7 +1,7 @@
 #
 # This file is part of LiteICLink.
 #
-# Copyright (c) 2017-2023 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2017-2024 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
 from migen import *
