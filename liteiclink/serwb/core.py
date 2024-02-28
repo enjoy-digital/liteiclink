d001e6e52980c2f782b234b0128d4f2bbf2aec16
Florent Kermarrec <florent@enjoy-digital.fr>
2024-02-28 14:24:57 +0100

    serwb/core: Add SERIO Core to allow tunneling inputs/outputs signals over our link (in additon of Wishbone).
    Useful for Control/Status/IRQs signals.

--- liteiclink/serwb/core.py
+++ liteiclink/serwb/core.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 from migen import *
+from migen.genlib.cdc import MultiReg
 
 from litex.gen import *
 
@@ -62,7 +63,7 @@ class SERWBCore(LiteXModule):
     def do_finalize(self):
         # Downstream Arbitration.
         # -----------------------
-        downstream_endpoints = [stream.Endpoint(packet_description(32))] * len(self.downstream_endpoints)
+        downstream_endpoints = [stream.Endpoint(packet_description(32)) for _ in range(len(self.downstream_endpoints))]
         for i, (k, v) in enumerate(self.downstream_endpoints.items()):
             self.comb += [
                 v.connect(downstream_endpoints[i], keep={"valid", "ready", "last", "data", "length"}),
@@ -71,7 +72,7 @@ class SERWBCore(LiteXModule):
         self.arbiter = Arbiter(
             masters = downstream_endpoints,
             slave   = self.packetizer.sink,
-            keep    = {"valid", "ready", "last", "data", "length"},
+            keep    = {"valid", "ready", "last", "data", "port", "length"},
         )
 
         # Upstream Dispatching.
@@ -84,3 +85,68 @@ class SERWBCore(LiteXModule):
         )
         for i, (k, v) in enumerate(self.upstream_endpoints.items()):
             self.comb += If(self.depacketizer.source.port == k, self.dispatcher.sel.eq(i))
+
+# SERIO Core ---------------------------------------------------------------------------------------
+
+class SERIOPacketizer(LiteXModule):
+    def __init__(self):
+        self.inputs = Signal(32)
+        self.source = source = stream.Endpoint(packet_description(32))
+
+        # # #
+
+        # Signals.
+        # --------
+        inputs        = Signal(32)
+        inputs_d      = Signal(32)
+
+        # Re-Synchronize Inputs.
+        # ----------------------
+        self.specials += MultiReg(self.inputs, inputs)
+
+        # Register Inputs.
+        # ----------------
+        self.sync += If(source.ready, inputs_d.eq(inputs))
+
+        # Generate Packet.
+        # ----------------
+        self.comb += [
+            source.valid.eq(inputs != inputs_d),
+            source.last.eq(1),
+            source.data.eq(inputs),
+            source.length.eq(4),
+        ]
+
+class SERIODepacketizer(LiteXModule):
+    def __init__(self):
+        self.sink    = sink = stream.Endpoint(packet_description(32))
+        self.outputs = Signal(32)
+
+        # # #
+
+        # Generate Outputs.
+        # -----------------
+        self.comb += sink.ready.eq(1)
+        self.sync += If(sink.valid & sink.last, self.outputs.eq(sink.data))
+
+class SERIOCore(LiteXModule):
+    def __init__(self, serwb_core):
+        self.inputs  = Signal(32)
+        self.outputs = Signal(32)
+
+        # # #
+
+        # Packetizer.
+        # -----------
+        self.packetizer = SERIOPacketizer()
+        self.comb += self.packetizer.inputs.eq(self.inputs)
+
+        # Depacketizer.
+        # -------------
+        self.depacketizer = SERIODepacketizer()
+        self.comb += self.outputs.eq(self.depacketizer.outputs)
+
+        # Add to SERWB Downstreams/Upstreams Endpoints.
+        # ---------------------------------------------
+        serwb_core.downstream_endpoints.update({1: self.packetizer.source})
+        serwb_core.upstream_endpoints.update(  {1: self.depacketizer.sink})
